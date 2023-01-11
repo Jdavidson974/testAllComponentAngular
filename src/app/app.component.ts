7735c2d77f5eaa9d1aadf3770ee1f68461f44009
Jdavidson <jd9743101@gmail.com>
2023-01-11 14:35:26 +0400

    system de login si pad de token dans le local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '@eartho/one-client-angular';
+import { catchError, tap } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +10,35 @@ import { AuthService } from '@auth0/auth0-angular';
 export class AppComponent {
   constructor(public auth: AuthService) { }
   title = 'testAuthGoogle';
+  @ViewChild('loginRef', { static: true }) loginElement!: ElementRef;
+  ngOnInit() {
+  }
+  loginWithRedirect() {
+    // si on a le token on fait qlq chose sinon
+    if (localStorage.getItem('token')) {
+      console.log(localStorage.getItem('token'));
+    } else {
+      // on fait la connection
+      this.auth.connectWithPopup({ access_id: "p9GGKAylYlcgij2oAqwi" }).pipe(
+        tap(
+          result => {
+            // dans le callback on recupere le token si il y en an un et on le met dans le local storage
+            this.auth.getIdToken().pipe(
+              tap(
+                token => {
+                  if (token) {
+                    localStorage.setItem('token', token);
+                  }
+                }
+              ),
+            ).subscribe();
+          }
+        )
+      ).subscribe();
+    }
+  }
+  showToken() {
+    // console.log(localStorage.getItem('token'));
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AuthModule } from '@auth0/auth0-angular';
+import { AuthModule } from '@eartho/one-client-angular';
 import { QRCodeModule } from 'angularx-qrcode';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -15,11 +14,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     QRCodeModule,
-    AuthModule.forRoot({
-      domain: 'dev-kqh08sd57e0x6q7d.us.auth0.com',
-      clientId: '8wTd1LrhVay3MCcirVotXLV18bAUYPYk'
-    }),
     NgbModule,
+    AuthModule.forRoot({
+      clientId: 'QuIge7dg8dvzmBtAGlzB',
+      domain: '',
+      enabledProviders: ['facebook', 'google',]
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
